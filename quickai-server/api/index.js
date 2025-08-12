f719df9821deb8ae2628ece53a3d8b4b8c1e62cd
gopalpayghan <gopaldadapayghan@gmail.com>
2025-08-12 08:25:05 +0530

    update index.js file

--- quickai-server/api/index.js
+++ quickai-server/api/index.js
@@ -2,10 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import { clerkMiddleware, requireAuth } from '@clerk/express'
-import aiRouter from './routes/aiRoutes.js'
-import connectCloudinary from './config/cloudinary.js'
-import userRouter from './routes/userRoutes.js'
-
+import aiRouter from '../routes/aiRoutes.js'
+import connectCloudinary from '../config/cloudinary.js'
+import userRouter from '../routes/userRoutes.js'
 
 const app = express()
 
@@ -15,18 +14,13 @@ app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
 
-// dotenv.config()
+app.get('/', (req, res) => res.send('PrismAI Backend API is Live!'))
+app.get('/api', (req, res) => res.send('PrismAI Backend API is Live!'))
 
-app.get('/', (req, res)=>res.send('Server is Live!'))
+app.use('/api/ai', requireAuth(), aiRouter)
+app.use('/api/user', requireAuth(), userRouter)
 
-app.use(requireAuth())
-
-app.use('/api/ai', aiRouter)
-app.use('/api/user', userRouter)
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, ()=>{
-    console.log('Server is running in port', PORT);
+// Export the Express app as a serverless function
+export default app
     
 })
